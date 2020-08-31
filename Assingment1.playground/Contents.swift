15c4a6768d43c4268debd304f7bb899bc32c5060
Elizabeth Etzkorn <elizabeth.etzkorn@smail.astate.edu>
2020-08-30 19:31:10 -0700

    Assignment 1

---
+++ Assingment1.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var coolWords = "Hello, Playground"
+
+var name = "My name is Lizz"
+
+var age = "I am "
+
+var integer = 27
+
+var integer2 = 25
+
+coolWords
+name
+age + String(integer)
+var string = "My daughter is "
+integer - integer2
+var paragraph = "My carreer aspirations are anything that does not involved ever using MacStadium again because it has taken me about 30 minutes to write these 20 lines due to the lag time"
